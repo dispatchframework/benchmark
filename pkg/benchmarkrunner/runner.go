a386b657317c31b8e508f434e69cedf07dc58cf1
Nicolaas Kaashoek <nick.kaashoek@gmail.com>
2018-06-14 12:20:16 -0700

    Beginnings of a unified script for running the tests

--- pkg/benchmarkrunner/runner.go
+++ pkg/benchmarkrunner/runner.go
@@ -24,6 +24,7 @@ func runScaling(config TestConfig) {
 	fmt.Printf("Running Scalability Tests: %v\n", config)
 	var testExec *exec.Cmd
 	test := fmt.Sprintf("%v/benchmarkscale.test", config.Location)
+<<<<<<< HEAD
 	shouldPlot := fmt.Sprintf("-plot=%v", config.Plot)
 	if config.Output == "" {
 		testExec = exec.Command(test, shouldPlot)
@@ -36,6 +37,17 @@ func runScaling(config TestConfig) {
 	// we don't want to report a test as failed in this case
 	if err != nil && err.Error() != "exit status 197" {
 		fmt.Printf("Error: %v, Output: %s\n", err, output)
+=======
+	if config.Output != "" {
+		testExec = exec.Command(test)
+	} else {
+		output := fmt.Sprintf("-outFile='./%v'", config.Output)
+		testExec = exec.Command(test, output)
+	}
+	output, err := testExec.Output()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+>>>>>>> Beginnings of a unified script for running the tests
 		panic("Failed to run the scaling tests")
 	}
 	fmt.Printf("Results of running tests: \n%s", output)
@@ -45,6 +57,7 @@ func runScaling(config TestConfig) {
 func runTiming(config TestConfig) {
 	fmt.Println("Running Timing Tests")
 	var testExec *exec.Cmd
+<<<<<<< HEAD
 	shouldPlot := fmt.Sprintf("-plot=%v", config.Plot)
 	test := fmt.Sprintf("%v/benchmarktiming.test", config.Location)
 	if config.Output != "" {
@@ -57,6 +70,18 @@ func runTiming(config TestConfig) {
 	if err != nil && err.Error() != "exit status 197" {
 		fmt.Printf("Error: %v, Output: %s", err, output)
 		panic("Failed to run the timing tests")
+=======
+	test := fmt.Sprintf("%v/benchmarktiming.test", config.Location)
+	if config.Output != "" {
+		testExec = exec.Command(test)
+	} else {
+		output := fmt.Sprintf("-outFile='./%v'", config.Output)
+		testExec = exec.Command(test, output)
+	}
+	output, err := testExec.Output()
+	if err != nil {
+		panic("Failed to run the scaling tests")
+>>>>>>> Beginnings of a unified script for running the tests
 	}
 	fmt.Printf("Results of running tests: \n%s", output)
 	fmt.Println("Finished timing scale tests")
